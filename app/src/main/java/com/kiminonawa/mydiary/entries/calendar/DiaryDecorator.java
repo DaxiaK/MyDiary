0164076db8038b6b0a8fc3e8d953a5aef74c651e
DA-XIA <daxia>
2017-03-15 23:01:09 +0800

    Try to add decorator

---
+++ app/src/main/java/com/kiminonawa/mydiary/entries/calendar/DiaryDecorator.java
@@ -0,0 +1,32 @@
+package com.kiminonawa.mydiary.entries.calendar;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
+import com.prolificinteractive.materialcalendarview.spans.DotSpan;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+/**
+ * Decorate several days with a dot
+ */
+public class DiaryDecorator implements DayViewDecorator {
+
+    private int color;
+    private HashSet<CalendarDay> dates;
+
+    public DiaryDecorator(int color, Collection<CalendarDay> dates) {
+        this.dates = new HashSet<>(dates);
+    }
+
+    @Override
+    public boolean shouldDecorate(CalendarDay day) {
+        return dates.contains(day);
+    }
+
+    @Override
+    public void decorate(DayViewFacade view) {
+        view.addSpan(new DotSpan(5, color));
+    }
+}
